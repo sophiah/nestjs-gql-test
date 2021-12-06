9f5fed834ceb6668879ba1d35b30e6bc5f3cc854
Sophia Hsieh <sophia.tw@gmail.com>
2021-12-06 16:41:51 +0800

    instrumentation-graphql

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('./open-telemetry');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
